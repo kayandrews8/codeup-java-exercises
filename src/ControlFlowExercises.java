91095415b9eef8b85cd39cc2333c30e5e53336d6
Kaylah Andrews <kandr716@gmail.com>
2022-03-28 16:25:41 -0500

    complete exercise 1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,23 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+//        int i = 5;
+
+//        while(i <= 15){
+//            System.out.println(i++);
+//        }
+//        int i = 0;
+
+//        do{
+//            System.out.println(i += 2);
+//        }while (i < 100);
+
+
+//        for(int i = 5; i <= 15; i ++){
+//            System.out.println(i);
+//        }
+
+        for(int i = 0; i <= 100; i += 2){
+            System.out.println(i);
+        }
     }
 }
