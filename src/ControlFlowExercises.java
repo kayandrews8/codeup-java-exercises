04827f5e168705c5afc39debfab36327c3a91ea7
Kaylah Andrews <kandr716@gmail.com>
2022-03-28 16:45:41 -0500

    correct do while statement

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,19 +5,19 @@ public class ControlFlowExercises {
 //        while(i <= 15){
 //            System.out.println(i++);
 //        }
-//        int i = 0;
+        int i = 2;
 
-//        do{
-//            System.out.println(i += 2);
-//        }while (i < 100);
+        do{
+            System.out.println(i *= 2);
+        }while (i < 65536);
 
 
 //        for(int i = 5; i <= 15; i ++){
 //            System.out.println(i);
 //        }
 
-        for(int i = 0; i <= 100; i += 2){
-            System.out.println(i);
-        }
+//        for(int i = 0; i <= 100; i += 2){
+//            System.out.println(i);
+//        }
     }
 }
