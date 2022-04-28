8aa5be80c14daba461732bce3a9bb12cad50e95c
Kaylah Andrews <kandr716@gmail.com>
2022-04-27 23:41:46 -0500

    create file to practice java

---
+++ src/Practice.java
@@ -0,0 +1,13 @@
+
+
+public class Practice {
+
+    public static int subtractTen(int num){
+        return num - 10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subtractTen(14));
+    }
+
+}
