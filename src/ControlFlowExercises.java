07aa1d51c62c12ae5e2aa73cd02fd59e8f4db889
Kaylah Andrews <kandr716@gmail.com>
2022-04-27 23:44:12 -0500

    practice with control flow

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,17 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        for (int i = 1; i <= 100; i += 1) {
+            if (((i % 3) == 0) & ((i % 5) == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else System.out.println(i);
 
+        }
     }
 }
 
-// for(int i = 1; i <= 100; i += 1){
-//         if(((i % 3) == 0) & ((i % 5) == 0)) {
-//         System.out.println("FizzBuzz");
-//         }else if(i % 3 == 0){
-//         System.out.println("Fizz");
-//         }else if(i % 5 == 0){
-//         System.out.println("Buzz");
-//         } else System.out.println(i);
 
 
